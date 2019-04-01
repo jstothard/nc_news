d3f88e57d3dab446bea6fdc950a00b9316016e41
jstothard <jake.stothard@arup.com>
2019-04-01 11:50:34 +0100

    added test and dev data

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,1 +1,8 @@
 // require in and export out all test data
+
+const articles = require("./data/articles");
+const comments = require("./data/comments");
+const topics = require("./data/topics");
+const users = require("./data/users");
+
+module.exports = { articles, comments, topics, users };
