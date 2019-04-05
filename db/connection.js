332a0a7b38cc12366c35ed144363011d660b11a9
jstothard <jake.stothard@arup.com>
2019-04-05 12:18:47 +0100

    update to allow hosting on heroku

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,6 @@
-const dbConfig = require('../knexfile');
+const dbConfig =
+  process.env.NODE_ENV === "production"
+    ? { client: "pg", connection: `${process.env.DATABASE_URL}?ssl=true` }
+    : require("../knexfile");
 
-module.exports = require('knex')(dbConfig);
+module.exports = require("knex")(dbConfig);
